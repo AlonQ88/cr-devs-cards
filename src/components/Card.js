e9367d54c57f93d00dcbe144395cf5b33f1f36cd
Alonso <alonq@outlook.com>
2020-12-10 19:39:27 -0600

    Added some test data

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,35 @@ import React from "react";
 
 class Card extends React.Component {
   render() {
+    const profile = testData[0];
     return (
       <div className="github-profile">
-        <img alt="placeholder" src="https://placehold.it/75"></img>{" "}
+        <img alt="placeholder" src={profile.avatar_url}></img>{" "}
         <div className="dev-info">
-          <div className="dev-name">Test Name</div>
-          <div className="company">Test Company</div>
+          <div className="dev-name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
     );
   }
 }
+
+const testData = [
+  {
+    name: "Dan Abramov",
+    avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
+    company: "@facebook",
+  },
+  {
+    name: "Sophie Alpert",
+    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+    company: "Humu",
+  },
+  {
+    name: "Sebastian Markbåge",
+    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
+    company: "Facebook",
+  },
+];
+
 export default Card;
