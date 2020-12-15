8185a8213606171e6fd2162680dc44c717533fed
Alonso <alonq@outlook.com>
2020-12-14 20:38:13 -0600

    Added Form component for user input

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form action="">
+        <input type="text" placeholder="GitHub Username" />
+        <button>Add Card</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
