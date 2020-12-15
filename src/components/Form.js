b49720129477ebda3b9a630e723c7cd1182c42a1
Alonso <alonq@outlook.com>
2020-12-14 20:50:12 -0600

    Added controled value on Form controller

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,23 @@
 import React from "react";
 
 class Form extends React.Component {
+  state = { userName: "" };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.userName);
+  };
+
   render() {
     return (
-      <form className="form" action="">
-        <input type="text" placeholder="GitHub Username" />
+      <form className="form" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="GitHub Username"
+          required
+          value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value })}
+        />
         <button>Add Card</button>
       </form>
     );
